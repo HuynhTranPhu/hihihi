79fb4a9fff618d7ef1c5c640d001fd2ea7b0250a
Huynh Tran Phu <huynhtranphu05121999@gmail.com>
2020-09-10 00:25:32 +0700

    complete fetch data from node server(React Hooks)

---
+++ backend/data.js
@@ -0,0 +1,94 @@
+export default {
+    products:
+    [
+        {
+            _id:'1',
+            name : 'Women T-shirt',
+            image: '/images/product1.jpg',
+            price: 60,
+            brand: 'Adidas',
+            description:'Nice T-shirt',
+            status:'In Stock',
+            rating: 4.6,
+            numReviews: 10
+        },
+        {
+            _id:'2',
+            name : 'Women T-shirt',
+            image: '/images/product1.jpg',
+            price: 60,
+            brand: 'Adidas',
+            description:'Nice T-shirt',
+            status:'In Stock',
+            rating: 4.6,
+            numReviews: 10
+        },
+        {
+            _id:'3',
+            name : 'Women T-shirt',
+            image: '/images/product1.jpg',
+            price: 60,
+            brand: 'Adidas',
+            description:'Nice T-shirt',
+            status:'In Stock',
+            rating: 4.6,
+            numReviews: 10
+        },
+        {
+            _id:'4',
+            name : 'Women T-shirt',
+            image: '/images/product1.jpg',
+            price: 60,
+            brand: 'Adidas',
+            description:'Nice T-shirt',
+            status:'In Stock',
+            rating: 4.6,
+            numReviews: 10
+        },
+        {
+            _id:'5',
+            name : 'Women T-shirt',
+            image: '/images/product1.jpg',
+            price: 60,
+            brand: 'Adidas',
+            description:'Nice T-shirt',
+            status:'In Stock',
+            rating: 4.6,
+            numReviews: 10
+        },
+        {
+            _id:'6',
+            name : 'Women T-shirt',
+            image: '/images/product1.jpg',
+            price: 60,
+            brand: 'Adidas',
+            description:'Nice T-shirt',
+            status:'In Stock',
+            rating: 4.6,
+            numReviews: 10
+        },
+        {
+            _id:'7',
+            name : 'Women T-shirt',
+            image: '/images/product1.jpg',
+            price: 60,
+            brand: 'Adidas',
+            description:'Nice T-shirt',
+            status:'In Stock',
+            rating: 4.6,
+            numReviews: 10
+        },
+        {
+            _id:'8',
+            name : 'Women T-shirt',
+            image: '/images/product1.jpg',
+            price: 60,
+            brand: 'Adidas',
+            description:'Nice T-shirt',
+            status:'In Stock',
+            rating: 4.6,
+            numReviews: 10
+        }
+        
+    ]
+}
